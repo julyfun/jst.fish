374c23e60d35dadbdac03a8f72855e6859cc3e70
julyfun <43675484+julyfun@users.noreply.github.com>
2023-08-28 12:45:56 +0800

    just push

--- utils.fish
+++ utils.fish
@@ -5,7 +5,8 @@ function jp
     && git push
 end
 
-function cmt
+# cmake make test
+function cmt --description 'cmake make test'
     cmake ..
     make -j8
     if test (count $argv) -ge 1
@@ -29,3 +30,17 @@ function just
         just.find $argv[2..-1]
     end
 end
+
+function mfa.open-link
+    switch (uname)
+    case Darwin
+        open -a "Google Chrome" $argv
+    case Linux
+    end
+end
+
+function hp.vim
+    mfa.open-link 'https://www.runoob.com/w3cnote/all-vim-cheatsheat.html'
+end
+
+
