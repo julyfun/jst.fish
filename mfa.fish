1a73db803989353eff075f91f2c2bd4475067dc1
julyfun <43675484+julyfun@users.noreply.github.com>
2024-03-20 23:14:43 +0800

    修复 __mfa.github-link 对于 .git 后缀识别的问题

--- mfa.fish
+++ mfa.fish
@@ -159,7 +159,10 @@ function __mfa.github-link
         set remote_relative_arg "tree/$branch/$relative_arg"
     end
     # 避免和 fish 内定义的临时 git 名冲突
-    command git remote -v | string match -rq 'github\.com:(?<user_repo>[\S]+)(\.git|\s)'
+    command git remote -v | string match -rq 'github\.com:(?<user_repo>[\S]+)\.git'
+    if test -z $user_repo
+        command git remote -v | string match -rq 'github\.com:(?<user_repo>[\S]+)\s'
+    end
     echo (string join -n '/' "https://github.com" $user_repo $remote_relative_arg)
 end
 
